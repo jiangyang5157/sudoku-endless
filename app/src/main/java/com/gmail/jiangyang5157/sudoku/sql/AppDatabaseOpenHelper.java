2886225c1bd091246fffbd54eb1d8eb55d5e3d43
Yang Jiang <jiangyang5157@gmail.com>
2016-12-10 01:35:14 +1300

    database version

--- app/src/main/java/com/gmail/jiangyang5157/sudoku/sql/AppDatabaseOpenHelper.java
+++ app/src/main/java/com/gmail/jiangyang5157/sudoku/sql/AppDatabaseOpenHelper.java
@@ -11,7 +11,7 @@ public class AppDatabaseOpenHelper extends BaseAppDatabaseOpenHelper {
      */
     public static final String DATABASE_NAME = "sudoku_endless.db";
 
-    public static final int DATABASE_VERSION = 9;
+    public static final int DATABASE_VERSION = 10;
 
     protected AppDatabaseOpenHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
